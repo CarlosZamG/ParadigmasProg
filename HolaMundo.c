003e92f9db29c69b4c8ef62b3783463517a7f525
Carlos <cdzamora02@outlook.es>
2022-02-17 21:32:42 -0600

    Se modificó el HolaMundo para mostrar el número de hilos

--- HolaMundo.c
+++ HolaMundo.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <omp.h>
+#include <unistd.h>
 
 int main(){
 
-    #pragma omp parallel
-    printf("Hola Mundo Paralelo \n");
+    int tid;
+
+    #pragma omp parallel private(tid) num_threads(2)
+    {
+
+        tid = omp_get_thread_num();
+        if(tid==0) sleep(tid+1);
+        printf("Hola Mundo Paralelo tid: %d \n",tid);
+
+    }
+    
 
 
 }
